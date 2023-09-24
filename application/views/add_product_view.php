acf464b0b350d5e8beeda69808b6a4c94d6cbc8f
KevinNicolaNatanael <kevinnicolanatanael@gmail.com>
2023-09-24 12:53:47 +0700

    Second commit

--- application/views/add_product_view.php
+++ application/views/add_product_view.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <title>Add New Product</title>
   <!-- load bootstrap css file -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/sb-admin-2.min.css" rel="stylesheet" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
 
 <body>
--- application/views/product_view.php
+++ application/views/product_view.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <title>Product List</title>
   <!-- load bootstrap css file -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <link rel="stylesheet"  href="<?php echo base_url() ?>assets/css/sb-admin-2.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
 
 <body>
